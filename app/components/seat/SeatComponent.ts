a90f8557a7b6cc3b777073f9def953ac5d3c373e
Kateryna Sinichkina <kateryna_sinichkina@epam.com>
2016-11-10 14:19:37 +0200

    shuttle block styled

--- app/components/seat/SeatComponent.ts
+++ app/components/seat/SeatComponent.ts
@@ -11,8 +11,20 @@ import "./seat.scss";
 
         <div class="shuttle">
           <div class="shuttle-root">
-            <div class="shuttle-cabin">
-              <div class="shuttle-seats"></div>
+            <div class="shuttle-cabin"></div>
+            <div class="shuttle-seats">
+              <div class="shuttle-seats-block">
+                <ul class="shuttle-seats__list">
+                  <li>1</li>
+                  <li>2</li>
+                  <li class="active">3</li>
+                  <li>4</li>
+                  <li>5</li>
+                  <li>6</li>
+                  <li>7</li>
+                  <li>8</li>
+                </ul>
+              </div>
               <div class="shuttle-door-up"></div>
               <div class="shuttle-door-down"></div>
             </div>
